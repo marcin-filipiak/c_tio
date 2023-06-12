070045568bde41e11fd4327245e78e8dd58211e1
marcin-filipiak <50583432+marcin-filipiak@users.noreply.github.com>
2023-06-12 08:36:36 +0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
  * tio - a simple TTY terminal I/O application
  *
  * Copyright (c) 2014-2016  Martin Lund
+ * modified by Marcin Filipiak
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
